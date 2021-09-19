e732972e8aca7335ce362aad42cd47231d824eb1
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2021-09-19 18:21:55 +0200

    feat(CesiumViewerDirective): viewer launches

--- src/app/cesium-viewer.directive.ts
+++ src/app/cesium-viewer.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ViewContainerRef } from '@angular/core';
+import { Viewer } from 'cesium';
 
 @Directive({
-  selector: '[appCesiumViewer]'
+  selector: '[cesiumViewer]'
 })
 export class CesiumViewerDirective {
 
-  constructor() { }
+  /** The cesium (engin) viewver reference */
+  private readonly _viewer: Viewer;
+
+  constructor(private ref: ViewContainerRef) {
+    // Init the viewer
+    this._viewer = new Viewer( this.ref.element.nativeElement);
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { buildModuleUrl } from 'cesium';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,10 @@ if (environment.production) {
   enableProdMode();
 }
 
+// Defininig Cesium base URL
+// @ts-ignore 
+window.CESIUM_BASE_URL = '/assets/cesium/';
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
